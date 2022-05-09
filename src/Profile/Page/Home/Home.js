4b33424c395e5dc20b07be1b20898d3a717732c0
unknown <trungbina3qo@gmail.com>
2022-05-09 14:58:58 +0700

    change profile

--- src/Profile/Page/Home/Home.js
+++ src/Profile/Page/Home/Home.js
@@ -1,17 +1,80 @@
 import React, { memo } from 'react'
 import clsx from 'clsx'
 import style from './home.module.scss'
+import project from '../../../data'
 
 function Home() {
   return (
     <footer className={clsx(style.contact)}>
       <h1 className={clsx(style.heading)}>my project</h1>
-      <div className={clsx(style.form ,"grid")}>
-        <div className={clsx("row")}>
-          
-        </div>
+      <div className={clsx(style.form, "grid")}>
+        {project.map((obj, index) => (
+          <div className={clsx("row", style.project)} key={obj.id}>
+            <div className={clsx("col", 'l-6', 'c-12', 'm-12')}>
+              {0 === (obj.id % 2)
+                ? <img src={obj.image} className={clsx(style.img)} />
+                : <>
+                  <h1 className={clsx(style.name)}>
+                    <label className={clsx(style.label)}>name:</label>
+                    <p className={clsx(style.des)}>{obj.name}</p>
+                  </h1>
+                  <h2 className={clsx(style.demo)}>
+                    <label className={clsx(style.label)}>demo:</label>
+                    <a className={clsx(style.des)}>{obj.demo}</a>
+                  </h2>
+                  <h2 className={clsx(style.descript)}>
+                    <label className={clsx(style.label)}>description:</label>
+                    <ul className={clsx(style.des)}>
+                      {obj.des.map((ob, index) => (
+                        <li key={index}>{ob}</li>
+                      ))}
+                    </ul>
+                  </h2>
+                  <h2 className={clsx(style.note)}>
+                    <label className={clsx(style.label)}>language:</label>
+                    <p className={clsx(style.des)}>{obj.language}</p>
+                  </h2>
+                  <h2 className={clsx(style.note)}>
+                    <label className={clsx(style.label)}>note:</label>
+                    <p className={clsx(style.des)}>{obj.note}</p>
+                  </h2>
+                </>
+              }
+            </div>
+            <div className={clsx("col", 'l-6', 'c-12', 'm-12')}>
+              {0 === (obj.id % 2)
+                ? <>
+                  <h1 className={clsx(style.name)}>
+                    <label className={clsx(style.label)}>name:</label>
+                    <p className={clsx(style.des)}>{obj.name}</p>
+                  </h1>
+                  <h2 className={clsx(style.demo)}>
+                    <label className={clsx(style.label)}>demo:</label>
+                    <a className={clsx(style.des)}>{obj.demo}</a>
+                  </h2>
+                  <h2 className={clsx(style.descript)}>
+                    <label className={clsx(style.label)}>description:</label>
+                    <ul className={clsx(style.des)}>
+                      {obj.des.map((ob, index) => (
+                        <li key={index}>{ob}</li>
+                      ))}
+                    </ul>
+                  </h2>
+                  <h2 className={clsx(style.note)}>
+                    <label className={clsx(style.label)}>language:</label>
+                    <p className={clsx(style.des)}>{obj.language}</p>
+                  </h2>
+                  <h2 className={clsx(style.note)}>
+                    <label className={clsx(style.label)}>note:</label>
+                    <p className={clsx(style.des)}>{obj.note}</p>
+                  </h2>
+                </>
+                : <img src={obj.image} className={clsx(style.img)} />
+              }
+            </div>
+          </div>
+        ))}
       </div>
-
     </footer>
   )
 }
--- src/data.js
+++ src/data.js
@@ -1,18 +1,26 @@
 const project = [
   {
-    name : 'loading',
-    demo : 'demo.com',
-    des : '1 số template loading ',
-    language : 'html, css, javascript',
-    note : 'chưa responsive'
+    id: 1,
+    image: "https://img.freepik.com/free-vector/loading-icon_167801-436.jpg?w=2000",
+    name: 'loading',
+    demo: 'demo.com',
+    des: ['1 số template loading '],
+    language: 'html, css, javascript',
+    note: 'chưa responsive'
   },
   {
-    name : 'store-shop',
-    demo : 'demo.com',
-    des : [
+    id: 2,
+    image: "https://s169.convertio.me/p/xMb8nozkqzmr81fd7v_jrw/bd3953d6f836da934054ea8bcbb59165/img1.webp",
+    name: 'store-shop',
+    demo: 'demo.com',
+    des: [
       'giao diện đơn giản, đẹp mắt, thân thiện với người dùng ',
       'menu sắp xếp khoa học, bố cục web rõ ràng',
       'thao tác thêm, xóa sản phẩm '
-    ]
+    ],
+    language: 'reactJS',
+    note: "chưa responsive"
   }
 ]
+
+export default project
